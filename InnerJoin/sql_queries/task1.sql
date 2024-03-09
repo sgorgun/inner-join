a2823dddcbe64f1c2f9652a16da09ab4d92db9a9
sgorgun <sergei.gorgun@gmail.com>
2024-03-09 16:42:59 +0200

    Ready

--- InnerJoin/sql_queries/task1.sql
+++ InnerJoin/sql_queries/task1.sql
@@ -1,1 +1,10 @@
-﻿
+﻿SELECT
+	product.id,
+	product_title.title,
+	product.price
+FROM
+	product
+	INNER JOIN
+	product_title
+	ON product.product_title_id = product_title.id
+ORDER BY product_title.title;
--- InnerJoin/sql_queries/task3.sql
+++ InnerJoin/sql_queries/task3.sql
@@ -1,1 +1,23 @@
-﻿
+﻿SELECT DISTINCT
+	product_title.title,
+	product.price
+FROM
+	product_title
+	INNER JOIN
+	product
+	ON product_title.id = product.product_title_id
+	INNER JOIN
+	product_category
+	ON product_title.product_category_id = product_category.id
+	INNER JOIN order_details
+	ON product.id = order_details.product_id
+	INNER JOIN customer_order
+	ON order_details.customer_order_id = customer_order.id
+	INNER JOIN customer
+	ON customer_order.customer_id = customer.person_id
+	INNER JOIN person
+	ON customer.person_id = person.id
+	WHERE person.birth_date BETWEEN '2000-01-01' AND '2010-12-31'
+
+ORDER BY product_title.title, product.price;
+
